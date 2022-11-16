97de0090b5412a3f9afbcc2b8357c301ad39a25b
ediaz-wallypos <emildsn00@gmail.com>
2022-11-15 23:58:18 -0300

    add simple array

---
+++ app/src/main/java/com/example/kotlin_algorithms_data_structures/Basic.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin_algorithms_data_structures
+
+fun main() {
+    val array = arrayOf("Kotlin", "Java", "C", "Typescript")
+    println(array[1])
+}
---
+++ app/src/main/java/com/example/kotlin_algorithms_data_structures/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_algorithms_data_structures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
