567eee472dd6f54d021996f3008f1406a8d68187
ediaz-wallypos <emildsn00@gmail.com>
2022-11-16 00:01:39 -0300

    add list

--- app/src/main/java/com/example/kotlin_algorithms_data_structures/Basic.kt
+++ app/src/main/java/com/example/kotlin_algorithms_data_structures/Basic.kt
@@ -3,4 +3,7 @@ package com.example.kotlin_algorithms_data_structures
 fun main() {
     val array = arrayOf("Kotlin", "Java", "C", "Typescript")
     println(array[1])
+
+    val readOnlyList = listOf<String>("Java", "Kotlin", "Nodejs", "Nest")
+    println("Readonly: $readOnlyList")
 }
